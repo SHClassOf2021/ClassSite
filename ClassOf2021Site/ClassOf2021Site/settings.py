b21e8538003214ea658dcfaf0329db6d8e479de1
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-07-14 22:24:31 -0400

    URLs adjusted for 'home' app. Shows landing page upon site url, no /.

--- ClassOf2021Site/ClassOf2021Site/settings.py
+++ ClassOf2021Site/ClassOf2021Site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -73,23 +74,23 @@ WSGI_APPLICATION = 'ClassOf2021Site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'mysql.connector.django',
-#         'NAME': 'shclassof2021',
-#         'USER': 'ClassOf2021Admin',
-#         'PASSWORD': 'ShepHillRams2021',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'shclassof2021',
+        'USER': 'ClassOf2021Admin',
+        'PASSWORD': 'ShepHillRams2021',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
