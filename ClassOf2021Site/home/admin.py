92745a6bc246933acbb5f4150921fa5ed9b4090e
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-07-15 12:59:16 -0400

    Create Announcement models for title and body. Accessible in the admin interface with proper naming.

--- ClassOf2021Site/home/admin.py
+++ ClassOf2021Site/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Announcement_Title, Announcement_Body
+
+admin.site.register(Announcement_Title)
+admin.site.register(Announcement_Body)
--- ClassOf2021Site/home/models.py
+++ ClassOf2021Site/home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Announcement_Title(models.Model):
+    title_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title_text
+
+    class Meta():
+        verbose_name = 'Announcement Title'
+
+class Announcement_Body(models.Model):
+    topic = models.ForeignKey(Announcement_Title, on_delete=models.CASCADE)
+    body_text = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.body_text
+
+    class Meta():
+        verbose_name = 'Announcement Body'
+        verbose_name_plural = 'Announcement Bodies'
