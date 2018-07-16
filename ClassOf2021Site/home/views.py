19d769d25a9e629a5feeddf47fb0f9e138d21513
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-07-15 22:11:34 -0400

    Basic templating for homepage and announcements page.

--- ClassOf2021Site/home/views.py
+++ ClassOf2021Site/home/views.py
@@ -7,7 +7,6 @@ from .models import Announcement
 def index(request):
     latest_announcements = Announcement.objects.order_by('-pub_date')[:3]
     context = {
-<<<<<<< HEAD
         'latest_announcements' : latest_announcements,
     }
     return render(request, 'home/index.html', context)
@@ -18,8 +17,3 @@ def announcements(request):
         'latest_announcements_all' : latest_announcements_all,
     }
     return render(request, 'home/announcements.html', context)
-=======
-        'latest_announcements':latest_announcements,
-    }
-    return render(request, 'home/index.html', context)
->>>>>>> 56f7fc63966f00dbe384c3108f0386d2ae3d279c
