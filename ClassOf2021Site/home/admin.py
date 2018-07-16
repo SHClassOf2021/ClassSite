3aedbb3f2da9fd881f99ee620e0c29cbdb31e555
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-07-15 20:28:25 -0400

    Trying out HTML templating with views.

--- ClassOf2021Site/home/admin.py
+++ ClassOf2021Site/home/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Announcement_Title, Announcement_Body
-
-admin.site.register(Announcement_Title)
-admin.site.register(Announcement_Body)
+from .models import Announcement
+admin.site.register(Announcement)
--- ClassOf2021Site/home/views.py
+++ ClassOf2021Site/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Announcement
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello people.')
+    latest_announcement_list = Announcement.objects.order_by('-pub_date')[:4]
+    context = {
+        'latest_announcement_list' : latest_announcement_list,
+    }
+    return (request, 'home/index.html', context)
