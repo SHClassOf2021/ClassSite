c953a29040b0b13742d3cd2743c38ea417a014ee
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-07-18 21:19:06 -0400

    Have working Google Gmail login, just need to limit to dcrsd.org.

--- ClassOf2021Site/ClassOf2021Site/settings.py
+++ ClassOf2021Site/ClassOf2021Site/settings.py
@@ -37,9 +37,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+
     'home',
     'contact',
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home:index'
+LOGOUT_REDIRECT_URL = 'home:index'
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
