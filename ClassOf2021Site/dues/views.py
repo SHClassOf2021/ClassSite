a48bf254dd0ed782a07d32da84a8747b258f0030
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-07-30 14:21:04 -0400

    Commit to fix login. Created 'dues' app.

---
+++ ClassOf2021Site/dues/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+
+gc = gspread.authorize(credentials)
+wks = gc.open('Class of 2021 Student E-mails and Dues').sheet1
+
+sheet1 = wks.get_all_records()
+
+def index(request):
+    user_email = User.objects.email
+    # user = wks.find(user_email)
+
+    return render(request, 'dues/index.html')
