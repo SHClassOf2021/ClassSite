e51e50a03134c83b3514192c520a1053153d2460
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-07-30 15:36:12 -0400

    Commit before i close up.

--- ClassOf2021Site/dues/views.py
+++ ClassOf2021Site/dues/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from users.models import CustomUser
 
 # Create your views here.
 
@@ -12,9 +13,9 @@ gc = gspread.authorize(credentials)
 wks = gc.open('Class of 2021 Student E-mails and Dues').sheet1
 
 sheet1 = wks.get_all_records()
+# wks.find(user_email)                       find a string
+# values_list = worksheet.col_values(1)      all values from column 1
 
 def index(request):
-    user_email = None
-    # user = wks.find(user_email)
 
     return render(request, 'dues/index.html')
