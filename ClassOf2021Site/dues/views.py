7af816d36ca07199342ae32eae4fcd8b469982bf
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-08-06 22:03:00 -0400

    Some updates.

--- ClassOf2021Site/dues/views.py
+++ ClassOf2021Site/dues/views.py
@@ -9,7 +9,7 @@ from .models import Dues
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('B:/client_secret.json', scope)
 
 gc = gspread.authorize(credentials)
 wks = gc.open('Class of 2021 Student E-mails and Dues').sheet1
