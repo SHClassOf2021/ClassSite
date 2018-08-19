c31ae8f0a307c77c7ff144b5daf0049281606797
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-08-18 22:42:41 -0400

    heroku

--- ClassOf2021Site/ClassOf2021Site/settings.py
+++ ClassOf2021Site/ClassOf2021Site/settings.py
@@ -188,6 +188,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
