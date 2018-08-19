8675ea17d665d2a27a2fb8d008aac708b47407dd
SHClassOf2021 <40905665+SHClassOf2021@users.noreply.github.com>
2018-08-19 12:25:28 -0400

    Revert "Before Heroku. Oh boy."
    This reverts commit 46d751b9eefe1d130e17ebd3e5a2d5ceb1edff2f.

--- ClassOf2021Site/ClassOf2021Site/settings.py
+++ ClassOf2021Site/ClassOf2021Site/settings.py
@@ -20,25 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '3_x#ka_=#81po^l5392xrh)!!gw74duw_eo8e!+5t2=v(@ys=a'
-
-import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3_x#ka_=#81po^l5392xrh)!!gw74duw_eo8e!+5t2=v(@ys=a')
+SECRET_KEY = '3_x#ka_=#81po^l5392xrh)!!gw74duw_eo8e!+5t2=v(@ys=a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-]
-
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -188,8 +175,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
